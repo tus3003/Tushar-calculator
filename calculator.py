f1e2cafa74dec3d1e081d0a7ecf1c26975fb3232
Tushar Takher <76086893+tus3003@users.noreply.github.com>
2023-02-05 14:51:11 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,29 @@
+#addition of two no.
+a = float(input("enter first no."))
+b = float(input("entwe second no."))
+add = a + b
+subtract = a-b
+divide = a/b
+multiplication = a*b
+value = a//b
+power = a**b
+# c = a + b
+# print(f"sum is{c} ")
+# ValueError: invalid literal for int() with base 10: '5.5' - inbuild exception class (run time error)
+z = input("choose what operation you want to perfome 'add', 'subtract', 'divide', 'multiplication', 'value', 'power'")
+
+if z == "add":
+    print("sum is",add)
+elif z == "subtract":
+    print("difference is",subtract)
+elif z == "divide":
+    print("difference is", divide)
+elif z == "multiplication":
+    print("difference is", multiplication)
+elif z == "value":
+    print("difference is", value)
+elif z == "power":
+    print("difference is", power)
+else:
+    print('please enter correct value')
+
